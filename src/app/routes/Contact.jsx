440278b8a1129af1d51a0c19b132b94e6c8dc93a
kostas23Github <kanastasiou.dev@gmail.com>
2025-01-19 18:17:02 +0200

    Updates

--- src/app/routes/Contact.jsx
+++
@@ -1,9 +0,0 @@
-
-function Contact() {
-  return (
-    <>
-    </>
-  )
-}
-
-export default Contact
--- src/components/contexts/ProductsContext.jsx
+++
@@ -1,7 +0,0 @@
-// The below code could be in the same directory with ProductsProvider but react recommends separating them & would throw this warning: 
-// !"Fast refresh only works when a file only exports components. Move your React context(s) to a separate file.eslint(react-refresh/only-export-components)".
-
-import { createContext } from "react";
-
-// Create the context that will store all fetched product data.
-export const ProductsContext = createContext();
