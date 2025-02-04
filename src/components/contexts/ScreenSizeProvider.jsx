bce71c6b28c673b15cc2dac76f477115161ecccf
kostas23Github <kanastasiou.dev@gmail.com>
2025-02-04 23:01:05 +0200

    Added more options to screenSizeContext & useScreenSize custom hook.

--- src/components/contexts/ScreenSizeProvider.jsx
+++ src/components/contexts/ScreenSizeProvider.jsx
@@ -7,19 +7,25 @@ const ScreenSizeContext = createContext();
 
 // Define breakpoints - might add more!
 const breakpoints = {
+  mobilePortrait: { maxWidth: 450 },
   mobile: { maxWidth: 767 },
   tablet: { minWidth: 768, maxWidth: 1023 },
-  desktop: { minWidth: 1024 },
+  laptop: { minWidth: 1024, maxWidth: 1279 },
+  desktop: { minWidth: 1280, maxWidth: 1535 },
+  monitor: { minWidth: 1536 },
 };
 
 // Provider Component
 export default function ScreenSizeProvider({ children }) {
+  const isMobilePortrait = useMediaQuery(breakpoints.mobilePortrait);
   const isMobile = useMediaQuery(breakpoints.mobile);
   const isTablet = useMediaQuery(breakpoints.tablet);
+  const isLaptop = useMediaQuery(breakpoints.laptop);
   const isDesktop = useMediaQuery(breakpoints.desktop);
+  const isMonitor = useMediaQuery(breakpoints.monitor);
 
   return (
-    <ScreenSizeContext.Provider value={{ isMobile, isTablet, isDesktop }}>
+    <ScreenSizeContext.Provider value={{ isMobilePortrait, isMobile, isTablet, isLaptop, isDesktop, isMonitor }}>
       {children}
     </ScreenSizeContext.Provider>
   );
@@ -27,7 +33,7 @@ export default function ScreenSizeProvider({ children }) {
 
 ScreenSizeProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 // Custom hook to consume context
 // Returns the values defined at the return statement above.
