5f9875c50d2b06aff39cde01006e85ef309370f1
kostas23Github <kanastasiou.dev@gmail.com>
2025-02-07 22:27:48 +0200

    A modal component which in this case handles the email form.

---
+++ src/components/FormModal.jsx
@@ -0,0 +1,32 @@
+import Button from "./button/Button";
+import { IoCloseSharp } from "react-icons/io5";
+import PropTypes from "prop-types";
+import Backdrop from "./Backdrop";
+
+function FormModal({ modalOpen }) {
+  return (
+    <>
+      <Backdrop onClickModal={modalOpen}/>
+      <div className="fixed top-1/2 -translate-x-1/2 left-1/2 -translate-y-1/2 z-50 aspect-video bg-slate-100 px-6 py-4 ring ring-gold/60 rounded-md shadow-lg shadow-gold flex justify-between items-center gap-8">
+        <span>Your email is submitted.</span>
+        <Button
+          variant="icon"
+          extraStyles="text-4xl block mx-auto p-2"
+          tooltipOptions={{
+            text: "Close",
+            position: "bottom",
+            distance: "100",
+          }}
+        >
+          <IoCloseSharp onClick={() => modalOpen(false)} />
+        </Button>
+      </div>
+    </>
+  );
+}
+
+FormModal.propTypes = {
+  modalOpen: PropTypes.func.isRequired,
+};
+
+export default FormModal;
