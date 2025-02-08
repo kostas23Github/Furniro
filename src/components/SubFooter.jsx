890450b89959c3468a1dd09b2d45b51c327965c6
kostas23Github <kanastasiou.dev@gmail.com>
2025-02-08 18:29:50 +0200

    Add SubFooter component.

---
+++ src/components/SubFooter.jsx
@@ -0,0 +1,9 @@
+function SubFooter() {
+  return (
+    <div className="mx-6 py-4 md:mx-10 md:py-6  border-t-2 border-grey-200">
+      <p>2025 furniro. All rights reserved</p>
+    </div>
+  )
+}
+
+export default SubFooter;
