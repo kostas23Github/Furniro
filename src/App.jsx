abcabde35947a5a1ff440db3e786e1721dfd1889
kostas23Github <kanastasiou.dev@gmail.com>
2025-02-12 22:06:05 +0200

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <Route path="/shop" element={<Shop />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/*" element={<ErrorPage />} />   
+        <Route path="*" element={<ErrorPage />} />   
       </Route>
     </Routes>
   );
