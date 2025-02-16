3d9e53d8fbb62d9aa10cf7046e3a0f965649c94e
kostas23Github <kanastasiou.dev@gmail.com>
2025-02-16 22:48:35 +0200

    Add new route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./app/Layout";
 import Home from "./app/routes/Home";
 import Shop from "./app/routes/Shop";
+import SingleProduct from "./app/routes/SingleProduct";
 import About from "./app/routes/About";
 import Contact from "./app/routes/Contact";
 import ErrorPage from "./app/routes/ErrorPage";
@@ -13,11 +14,18 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="shop" element={<Shop />} />
+        <Route
+          path="shop/:productId"
+          element={<SingleProduct />}
+        />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
         <Route path="404" element={<ErrorPage />} /> {/*Match the redirect */}
-        <Route path="*" element={<ErrorPage message="Please check the URL."/>} />   
+        <Route
+          path="*"
+          element={<ErrorPage message="Please check the URL." />}
+        />
       </Route>
     </Routes>
   );
