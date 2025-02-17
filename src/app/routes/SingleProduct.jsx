2d712cb9733f5771d0da50d80496b2eeeddd55d0
kostas23Github <kanastasiou.dev@gmail.com>
2025-02-17 21:57:46 +0200

    Added most of product details.

--- src/app/routes/SingleProduct.jsx
+++ src/app/routes/SingleProduct.jsx
@@ -1,14 +1,22 @@
 import { useParams } from "react-router-dom";
 import { useContext, useState } from "react";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaSquareXTwitter } from "react-icons/fa6";
 import { ProductsContext } from "../../components/contexts/ProductsContext";
 import Loading from "../../components/loadingAnimation/Loading";
 import ErrorPage from "./ErrorPage";
 import StarRating from "../../components/StarRating";
+import useHover from "../../components/hooks/useHover";
+import Tooltip from "../../components/button/tooltip";
+import Button from "../../components/button/Button";
 
 function SingleProduct() {
   const { productId } = useParams();
   const { products, loading, error } = useContext(ProductsContext);
+  const [hoverRef, isHovered] = useHover();
   const [currentImgIndex, setCurrentImgIndex] = useState(0);
+  const [productBgColor, setProductBgColor] = useState("gold");
 
   if (loading) return <Loading />;
   if (error) return <ErrorPage />; // Add a message here.
@@ -47,16 +55,95 @@ function SingleProduct() {
           <img
             src={images[currentImgIndex]}
             alt={product.title}
-            className="bg-gold-light-2 hover:bg-gold-light-3 rounded-lg shrink w-full max-w-[423px] aspect-[0.846]"
+            className={`bg-${productBgColor} rounded-lg shrink w-full max-w-[423px] aspect-[0.846]`}
           />
         </div>
-        <div className="product-info-container">
-          <p className="text-3xl w-max">{product.title}</p>
-          <p className="w-max">{product.price}</p>
-          <div className="flex items-center">
-            <StarRating rating={product.rating}/> 
-            <p className="ms-2 ps-4 me-2 text-grey-400 border-l-2 border-grey-200">{product.reviews.length}</p><p className="text-grey-400">Customer Reviews</p>
+        <div className="product-info-container md:py-6">
+          <p className="text-3xl w-max mb-3 md:mb-5">{product.title}</p>
+          <p className="w-max">{product.price}€</p>
+          <div className="flex items-center my-3">
+            <StarRating rating={product.rating} />
+            <p className="ms-2 ps-4 me-2 text-grey-400 border-l-2 border-grey-200">
+              {product.reviews.length}
+            </p>
+            <p className="text-grey-400">Customer Reviews</p>
+          </div>
+          <div>
+            <p>{product.description}</p>
+          </div>
+          <div ref={hoverRef} className="w-max relative my-4">
+            <p className="text-grey-400 mb-2">Color</p>
+            <div className="flex gap-2 lg:gap-3">
+              <div
+                className="bg-purple-600 w-8 h-8 rounded-full cursor-pointer"
+                onClick={() => setProductBgColor("purple-600")}
+              ></div>
+              <div
+                className="bg-slate-800 w-8 h-8 rounded-full cursor-pointer"
+                onClick={() => setProductBgColor("slate-800")}
+              ></div>
+              <div
+                className="bg-gold w-8 h-8 rounded-full cursor-pointer"
+                onClick={() => setProductBgColor("gold")}
+              ></div>
             </div>
+            {isHovered && (
+              <Tooltip text="Set bg color" position="bottom" distance="120" />
+            )}
+          </div>
+          <div className="flex gap-4 md:gap-6 lg:gap-8">
+            <div className="cart-actions-container flex justify-between w-[106px] md:w-[130px] lg:w-[150px] outline outline-2 outline-gray-800 rounded-lg px-3 py-2 md:px-5 md:py-4 hover:bg-gold-light-2">
+              <Button variant="text">-</Button>
+              <p>1</p>
+              <Button variant="text">+</Button>
+            </div>
+            <Button
+              variant="secondary-reversed"
+              extraStyles="w-[106px] md:w-[130px] lg:w-[150px] px-3 py-2 rounded-lg"
+            >
+              Add to Cart
+            </Button>
+          </div>
+          <hr className="my-8 lg:my-12" />
+          <div className="extra-info text-grey-400">
+            <div className="flex mb-1">
+              <p className="w-32">SKU</p>
+              <div className="flex">
+                <span className="me-3">:</span>
+                <span>{product.sku}</span>
+              </div>
+            </div>
+            <div className="flex mb-1">
+              <p className="w-32">Category</p>
+              <div className="flex">
+                <span className="me-3">:</span>
+                <span>{product.category}</span>
+              </div>
+            </div>
+            <div className="flex mb-1">
+              <p className="w-32">Tags</p>
+              <div>
+                <span className="me-3">:</span>
+                {product.tags.map((tag, index) => (
+                  <span key={index}>
+                    {tag}
+                    {index < product.tags.length - 1 && ", "}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="flex mb-1">
+              <p className="w-32">Share</p>
+              <div className="flex items-center">
+                <span className="me-3">:</span>
+                <div className="flex">
+                  <FaFacebook className="text-grey-700 text-[21px]" />
+                  <AiFillInstagram className="text-grey-700 text-[21px]" />
+                  <FaSquareXTwitter className="text-grey-700 text-[21px]" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
