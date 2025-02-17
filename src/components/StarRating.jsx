e001038b292805f7a7f58f60c07e9dfeb758c60c
kostas23Github <kanastasiou.dev@gmail.com>
2025-02-17 21:56:09 +0200

    Add tooltip to StarRating component & implemented useHover hook.

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -2,14 +2,20 @@
 
 import PropTypes from "prop-types";
 import { FaStar } from "react-icons/fa";
+import useHover from "./hooks/useHover";
+import Tooltip from "./button/tooltip";
 
 function StarRating({ rating }) {
+  const [hoverRef, isHovered] = useHover();
   const totalStars = Math.ceil(rating);
+  
   return (
-    <div className="flex gap-1 w-max">
+    <div ref={hoverRef} className="flex gap-1 w-max relative">
+      {isHovered && (
+        <Tooltip text={rating.toString()} position="bottom" distance="100" />
+      )}
       {Array.from({ length: totalStars }).map((_, index) => {
         const fillPercentage = Math.max(0, Math.min(1, rating - index)) * 100; // Limit from 0 to 100%
-
         return (
           <div key={index} className="relative w-6 h-6">
             <FaStar
