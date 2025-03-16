4b89aff04b6c64d21c3a7be4270680215e5092ea
kostas23Github <kanastasiou.dev@gmail.com>
2025-03-16 20:35:24 +0200

    Update Subscribe to newsletter component to align with other form-related components.

---
+++ src/components/form/SubscribeForm.jsx
@@ -0,0 +1,66 @@
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import Button from "../button/Button";
+import FormModal from "./FormModal";
+
+function SubscribeForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+  } = useForm({ reValidateMode: "onChange" });
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMsg, setModalMsg] = useState(""); // Store modal message in state
+
+  function onSubmit(data) {
+    console.log(data);
+    // data contains the currently submitted form values as an object.
+    const subscribedUsers =
+      JSON.parse(localStorage.getItem("subscribedUsers")) || [];
+
+      console.log(subscribedUsers);
+      
+    // Find the user by email
+    const existingUser = subscribedUsers.find(
+      (user) => user.toLowerCase() === data.email.toLowerCase()
+    );
+
+    if (existingUser) {
+      // call modal with msg sth like "already subscribed"
+      setModalMsg("You have already subscribed!");
+    } else {
+      subscribedUsers.push(data.email);
+      setModalMsg("Your email is submitted!");
+      localStorage.setItem("subscribedUsers", JSON.stringify(subscribedUsers));
+    }
+
+    setIsModalOpen(true);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="email"
+        {...register("email", {
+          required: "E-mail is required",
+          pattern: {
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            message: "Invalid email address",
+          },
+          onChange: () => trigger("email"),
+        })}
+        placeholder="Enter Your Email Address"
+        className="block bg-transparent placeholder:italic border-b-4 focus:border-grey-400 me-2 focus:outline-0"
+      />
+      {errors.email && (
+        <p className="text-red text-xs mt-1">{errors.email.message}</p>
+      )}
+      <Button type="submit">SUBSCRIBE</Button>
+      {isModalOpen && <FormModal setIsModalOpen={setIsModalOpen} message={modalMsg}/>}
+    </form>
+  );
+}
+
+export default SubscribeForm;
