6f58ef9d31020251142c8a8178680e7c9800f232
kostas23Github <kanastasiou.dev@gmail.com>
2025-03-20 16:22:23 +0200

    Added dark theme to Loading component via consuming ThemeContext.

--- src/components/loadingAnimation/Loading.jsx
+++ src/components/loadingAnimation/Loading.jsx
@@ -1,10 +1,13 @@
 import SpinnerSVG from "./SpinnerSVG";
-
+import { useContext } from "react";
+import ThemeContext from "../contexts/ThemeContext";
 // 92.8px is the height of the NavBar
 function Loading() {
+    const { theme } = useContext(ThemeContext);
+
   return (
-    <div className="bg-slate-50 h-[calc(100vh-92.8px)] flex justify-center items-center">
-      <SpinnerSVG mode="light" size="100" />
+    <div className="bg-slate-50 dark:bg-slate-700 h-[calc(100vh-92.8px)] flex justify-center items-center">
+      <SpinnerSVG mode={theme} size="100" />
     </div>
   );
 }
